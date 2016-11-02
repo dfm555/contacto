6768ea6e91e3c1c7fd20080581ac4190b759b6dc
Yording <yording9@gmail.com>
2016-11-02 14:44:03 -0500

    se conectan los metodos de: -punto 2a -punto 2c -punto 2d Se añaden las interfaces de: -dialogo buscar por nombre -eliminar un contacto

---
+++ src/Interfaz/DialogoBuscarContacto.java
@@ -0,0 +1,69 @@
+package Interfaz;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+
+/**
+ * Formulario para agregar un libro al cat�logo
+ */
+public class DialogoBuscarContacto extends JDialog  implements ActionListener{
+    private final JLabel lblnombre;
+    private final JTextField txtnombre;
+    private final JButton btnaceptar;
+    private final InterfazDirectorioContactos ventanaPrincipal;
+
+    public DialogoBuscarContacto(InterfazDirectorioContactos interfaz) {
+        ventanaPrincipal = interfaz;
+
+        setLayout(new GridLayout(1, 3));
+        setPreferredSize(new Dimension(300, 50));
+
+        lblnombre = new JLabel("Nombre ");
+        txtnombre = new JTextField();
+        btnaceptar = new JButton("Buscar");
+        btnaceptar.setActionCommand("BUSCAR");
+        btnaceptar.addActionListener(this);
+
+        add(lblnombre);
+        add(txtnombre);
+        add(btnaceptar);
+
+
+        setTitle("Buscar Contacto por el Nombre");
+        pack();
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    public void accionAlBuscar() {
+       String nombre = txtnombre.getText();
+      
+        if (nombre == null || nombre.equals("")) {
+            JOptionPane.showMessageDialog(this, "El nombre no puede ser vacío", "Buscar un Contacto", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        ventanaPrincipal.buscarContactoPorNombre(nombre);
+        txtnombre.setText("");
+        dispose();
+
+    }
+ 
+    public void actionPerformed(ActionEvent evento) {
+        String actionCommand = evento.getActionCommand();
+
+        // Bot�n buscar
+        if (actionCommand.equals("BUSCAR")) {
+            accionAlBuscar();
+        }
+    }
+}
